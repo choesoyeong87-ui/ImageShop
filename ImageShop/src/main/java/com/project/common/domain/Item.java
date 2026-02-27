0d15ffbec5d6a4e2adeca22aa983af86c796ecea
choesoyeong87-ui <choesoyeong87@gmail.com>
2026-02-27 16:01:43 +0900

    [추가] 상품관리

---
+++ ImageShop/src/main/java/com/project/common/domain/Item.java
@@ -0,0 +1,16 @@
+package com.project.common.domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+@Data
+public class Item {
+	private Integer itemId; 
+	private String itemName; 
+	private Integer price; 
+	private String description; 
+	private MultipartFile picture; 
+	private String pictureUrl; 
+	private MultipartFile preview; 
+	private String previewUrl;
+}
